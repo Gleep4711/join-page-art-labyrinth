95a98915a1ee6371c74592ac6ea90591d7a99bd4
Gleep4711 <gleep4711@gmail.com>
2025-03-27 01:20:28 +0200

    Add url postgreSQL login to alembic/env.py for debugging

--- back/alembic/env.py
+++ back/alembic/env.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from logging.config import fileConfig
 
 from alembic import context
@@ -14,6 +15,11 @@ Base: DeclarativeMeta
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+
+# DEBUG
+print(f"postgres_url: {app_config.postgres_url}")
+logging.info(f"postgres_url: {app_config.postgres_url}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
